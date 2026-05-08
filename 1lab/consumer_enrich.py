0c0dcf69787d354bbb4dbb248301422d06178f94
wikimatyj <wmatyjaszczyk2@gmail.com>
2026-05-08 15:06:30 +0200

    Update consumer_enrich.py

--- 1lab/consumer_enrich.py
+++ 1lab/consumer_enrich.py
@@ -4,6 +4,8 @@ import json
 consumer = KafkaConsumer(
     'transactions',
     bootstrap_servers='broker:9092',
+    auto_offset_reset='earliest',
+    group_id='enrich-group',
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
@@ -17,11 +19,5 @@ def get_risk_level(amount):
 
 for message in consumer:
     tx = message.value
-    
-
-    tx['risk_level'] = get_risk_level(tx['amount'])
-    
-    print(
-        f"{tx['tx_id']} | {tx['amount']:.2f} PLN | "
-        f"{tx['store']} | risk: {tx['risk_level']}"
-    )
+    tx['risk_level']= get_risk_level(tx['amount'])
+    print(f"[{tx['risk_level']:6s}] {tx['tx_id']} | {tx['amount']:.2f} PLN")
